65513daae430637fb2cfdb900b75d90a63cde1e3
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2019-02-18 10:09:39 -0800

    successfully able to change the integers in each tic tac toe spot on the number pad, however, instead of replacing number with the user's character, A 9 displays.

--- Practice_Modules/Projects/project1/project1.py
+++ Practice_Modules/Projects/project1/project1.py
@@ -1,6 +1,16 @@
 #import function
 # from IPython.display import clear_output
 
+#Define global variables
+response_one = None
+player_one_response = None
+player1_marker = None
+player2_marker = None
+
+filled_board = ['#','1','2','3','4','5','6','7','8','9']
+
+location = None
+
 #1. 
 #Create a function that takes a list, board_list, as an argument
 def print_board(board_list):
@@ -16,7 +26,7 @@ def print_board(board_list):
 
 
 #2. Create a function that will take the input of user one and assign characters to player1 and player2
-def user_input():
+def user_input(response_one, player1_marker, player2_marker):
     #Create a variable with an empty string bc it will eventually hold value
     response_one = ''
 
@@ -44,8 +54,8 @@ def user_input():
     
     return(player1_marker,player2_marker)
 
-#3. Create a function that matches the indeces of the list passed to the function, board_list, to integers called , num.
 
+#3. Create a function that matches the indeces of the list passed to the function, board_list, to integers called , num.
 def get_location(board_list):
     for index in range(1,10):
         location = board_list[index]
@@ -54,12 +64,14 @@ def get_location(board_list):
 
     print("You chose for location the number:", player_one_response)
 
-    numbers = [1,2,3,4,5,6,7,8,9]
+    change_marker(player_one_response, board_list, player1_marker, location, player2_marker)
 
-    for num in numbers:
-        if (player_one_response == num):
-            print("That is a number on the number pad.")
-            break
+    # numbers = [1,2,3,4,5,6,7,8,9]
+
+    # for num in numbers:
+    #     if (player_one_response == num):
+    #         print("That is a number on the number pad.")
+    #         break
 
     # sub-TROUBLESHOOT
     # if (player_one == 1):
@@ -70,21 +82,55 @@ def get_location(board_list):
     #     print("That aint it")
 
     print_board(board_list)
+    print("Board printed again")
 
     return (player_one_response,location)
 
+# def location_confirm(player_one_response, location):
+
+#4. Create function that changes the marker on the board
+def change_marker(player_one_response, board_list, location, player1_marker, player2_marker):
+
+    print("This should player1_marker", player1_marker)
+    # print("This should be player2_marker", player2_marker)
+# def change_marker(board_list):
+    if (player_one_response == 1):
+    # if (player_one_response == 1):
+        print("Success in matching.")
+        board_list[player_one_response] = player1_marker
+        print("Successfully changed number to marker.")
     
+    if (player_one_response == 2):
+    # if (player_one_response == 1):
+        print("Success in matching.")
+        board_list[player_one_response] = player1_marker
+        print("Successfully changed number to marker.")
+    
+    
+    
+
+    
+    # print(board_list)
+
+
 
     
     
 
     
 
-#RUNNING CODE
-user_input()
-filled_board = ['#','1','2','3','4','5','6','7','8','9']
-print_board(filled_board)
-get_location(filled_board)
+#Collect smaller functions into this function
+def main_function(response_one, player1_marker, location, player2_marker, player_one_response, filled_board):
+    user_input(response_one, player1_marker,player2_marker)
+    print_board(filled_board)
+    get_location(filled_board)
+    change_marker(player_one_response, filled_board, location, player1_marker, player2_marker)
+    # change_marker()
+
+
+#DRIVER CODE
+main_function(response_one, player1_marker, player2_marker, player_one_response, location, filled_board)
+
     
 
         
