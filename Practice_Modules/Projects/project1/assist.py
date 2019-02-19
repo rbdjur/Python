77307fc73ef3a1bea4fe3516aef90b96ef94d7a3
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2019-02-19 10:28:00 -0800

    restart.py is my second attempt to clean-up code from project1.py with a different approach to handling the data input by user one and user two.

--- Practice_Modules/Projects/project1/assist.py
+++ Practice_Modules/Projects/project1/assist.py
@@ -1,4 +1,111 @@
-#   for index in range(1,10):
-#         for num in range(1,10):
-#             num = board_list[index]
-#             print(num)
+filled_board = ['#','1','2','3','4','5','6','7','8','9']
+
+location_list = []
+character_list = []
+
+
+def print_board(board_list):
+    print(board_list[7] + " " + '|' + " " + board_list[8] + " " + '|' + " " + board_list[9])
+    print(board_list[4] + " " + '|' + " " + board_list[5] + " " + '|' + " " + board_list[6])
+    print(board_list[1] + " " + '|' + " " + board_list[2] + " " + '|' + " " + board_list[3])
+
+def input_mark_num():
+    print("Hello Player one, please choose a location (1-9) and symbol (X or O) separated by a space: ")
+    mark_and_num = input("").split()
+    print(mark_and_num)
+    print("location", mark_and_num[0])
+    print("character", mark_and_num[1])
+
+    #This line may cause bug issues. May need to comment this out
+    player1 = mark_and_num[1]
+
+    if (player1 == "X"):
+        #line above, may need to replace player1 with mark_and_num[1]
+        print("Player one is X")
+        player2 = "O"
+        print("Player two is O")
+    else:
+        player2 = "X"
+    
+    #Put characters inside the list, character_list
+    character_list.append(player1)
+    character_list.append(player2)
+
+    print("CHECK TO SEE IF THESE CHARACTERS SWITCH", character_list)
+
+    #Put the number inside location
+    location_list = mark_and_num[0]
+
+    print("This is the location_list", location_list)
+   
+    
+    print(f"Player one is {player1} and Player two is {player2}")
+
+    print_board(filled_board)
+
+    return(filled_board, location_list, player1, player2)
+
+def change_marker(filled_board, l):
+    #l is the place holder for:
+    # l = input_mark_num()
+    #l is our access to all the variables within the input_mark_num() function
+    print("inside the change_marker function")
+
+    print("These are all the variables from input_and_num", l)
+
+    # print("This is the filled_board variable", l[0])
+    # print("This is the player one response", l[1])
+    # print("This is the player two response", l[2])
+    # print("This is the location_list", l[3])
+    board_list = l[0]
+    board_coordinate = l[1]
+    player1_marker = l[3]
+    player2_marker = l[2]
+    
+
+    print("This is the filled_board variable", board_list)
+    print("This is the player one response", player1_marker)
+    print("This is the player two response", player2_marker)
+    print("This is the location_list", board_coordinate)
+
+    print("This is filled board", filled_board)
+
+
+
+
+
+
+
+
+
+
+
+def win(board_list, mark_and_num):
+    print("Inside win function.")
+    if (board_list[7] == board_list[8] == board_list[9] == mark_and_num):
+        print("Win")
+
+    if (board_list[7] == board_list[4] == board_list[1] == mark_and_num):
+        print("Win")
+
+    if (board_list[1] == board_list[2] == board_list[3] == mark_and_num):
+        print("Win")
+
+    if (board_list[3] == board_list[6] == board_list[9] == mark_and_num):
+        print("Win")
+
+    if (board_list[4] == board_list[5] == board_list[6] == mark_and_num):
+        print("Win")
+
+    if (board_list[8] == board_list[5] == board_list[2] == mark_and_num):
+        print("Win")
+
+    if (board_list[1] == board_list[5] == board_list[9] == mark_and_num):
+        print("Win")
+    
+    if (board_list[7] == board_list[5] == board_list[3] == mark_and_num):
+        print("Win")
+
+print_board(filled_board)
+l = input_mark_num()
+change_marker(filled_board, l)
--- Practice_Modules/Projects/project1/project1.py
+++ Practice_Modules/Projects/project1/project1.py
@@ -9,26 +9,25 @@ player2_marker = None
 
 filled_board = ['#','1','2','3','4','5','6','7','8','9']
 
-location = None
+# location = None
 
 #1. 
 #Create a function that takes a list, board_list, as an argument
 def print_board(board_list):
-    # #Prompt Player One to pick a character
-    # user_input()
-
-    #print the indeces of the passed list, board_list, with pipe character |' separating each index.  Place the indeces in rows of 3
- 
     print(board_list[7] + " " + '|' + " " + board_list[8] + " " + '|' + " " + board_list[9])
     print(board_list[4] + " " + '|' + " " + board_list[5] + " " + '|' + " " + board_list[6])
     print(board_list[1] + " " + '|' + " " + board_list[2] + " " + '|' + " " + board_list[3])
+    # print(f"" + board_list[7] + " " + '|' + " " + board_list[8] + " " + '|' + " " + board_list[9])
+    # print(f"" + board_list[4] + " " + '|' + " " + board_list[5] + " " + '|' + " " + board_list[6])
+    # print(f"" + board_list[1] + " " + '|' + " " + board_list[2] + " " + '|' + " " + board_list[3])
+    
 
 
 
 #2. Create a function that will take the input of user one and assign characters to player1 and player2
 def user_input(response_one, player1_marker, player2_marker):
     #Create a variable with an empty string bc it will eventually hold value
-    response_one = ''
+    # response_one = ''
 
     #Create a while loop that will continue to ask the user to log their character
     while (response_one != "O" and response_one != "X"):
@@ -39,9 +38,10 @@ def user_input(response_one, player1_marker, player2_marker):
             print("That is not a valid answer. Try again.")
         
     
-    #Once the user logs their response and it is stored in the variable, response. Now we can create conditional statements off that response to assign the character of the second player.
+    #Collect the user response and store it in the variable, response_one. 
     player1_marker = response_one
-    # player2 = ' '
+
+    # Now we can create conditional statements off that response to assign the character of the second player.
     if (player1_marker == "X"):
         player2_marker = "O"
     else:
@@ -53,10 +53,12 @@ def user_input(response_one, player1_marker, player2_marker):
     return(player1_marker,player2_marker)
 
 
-#3. Create a function that matches the indeces of the list passed to the function, board_list, to integers called , num.
+#3. Create a function matching the indeces of the list passed to the function, board_list, to integers called , num.
 def get_location(board_list, player1_marker, player2_marker):
 
-    # player1_character = player1_marker
+    player1_marker = player1_marker
+
+    print("CHECK THIS", player1_marker)
 
     for index in range(1,10):
         location = board_list[index]
@@ -64,10 +66,20 @@ def get_location(board_list, player1_marker, player2_marker):
         
     player_one_response = int(input("Player One please choose a number to place your character that corresponds to the location on the number pad: "))
 
-    print("You chose for location the number:", player_one_response)
+    print(f"Player one chose {player_one_response} for the location")
 
 
-    change_marker(player_one_response, board_list, player1_marker, location, player2_marker)
+    change_marker_player_one(player_one_response, board_list,player1_marker)
+    #Think about changing the variable passed to change_marker to response to see if the program still runs without errros
+
+
+    player_two_response = int(input("Player Two please choose a number to place your character that corresponds to the location on the number pad: "))
+
+    print(f"Player two chose {player_two_response} for the location.")
+
+    change_marker_player_two(player_two_response, board_list, player2_marker)
+
+
 
     # numbers = [1,2,3,4,5,6,7,8,9]
 
@@ -92,83 +104,165 @@ def get_location(board_list, player1_marker, player2_marker):
 
 #4. Create function that changes the marker on the board
 # def change_marker(player_one_response, board_list, location, player1_marker, player2_marker):
-def change_marker(player_one_response, board_list, location, player1_marker, player2_marker):
-    print("Inside change_marker")
+
+# def change_marker_player_one(player1_marker, player_one_response, board_list):
+def change_marker_player_one(player_one_response, board_list,player1_marker):
+#Think about changing the variable, player_one_response, passed to change_marker to, response, to see if the program still runs without errros
+    print("Inside change_marker_player_one")
     print("HEY this is player_one_response", player_one_response)
+    print("this is player1_marker", player1_marker)
 
     if(player_one_response == 1):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
 
     if(player_one_response == 2):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        # print("this should be X", player1_marker)
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
     
     if(player_one_response == 3):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
     
     if(player_one_response == 4):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
     
     if(player_one_response == 5):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
     
     if(player_one_response == 6):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
     
     if(player_one_response == 7):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
     
     if(player_one_response == 8):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
     
     if(player_one_response == 9):
         print("inside the if statement")
-        player1_marker = "X"
-        print("this should be X", player1_marker)
+        # player1_marker = "X"
+        print("this should be what player one chose as their character", player1_marker)
         board_list[player_one_response] = player1_marker
+
+
+    # print("Inside change_marker now for user 2 choice")
+    # print("YO this is player_two_response", player_two_response)
     
+    # if(player_two_response == 9):
+    #     print("inside the if statement")
+    #     player2_marker = "O"
+    #     print("this should be O", player2_marker)
+    #     board_list[player_two_response] = player2_marker
+    print("updated board")
+    print_board(board_list)
+
+
+
+def change_marker_player_two(player_two_response, board_list, player2_marker):
+#Think about changing the variable, player_one_response, passed to change_marker to, response, to see if the program still runs without errros
+    print("Inside change_marker_player_two")
+    print("HEY this is player_one_response", player_two_response)
+
+    if(player_two_response == 1):
+        print("inside the if statement")
+        # player2_marker = "O"
+        print("this should be what player one chose as their character", player2_marker)
+        # print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+
+    if(player_two_response == 2):
+        print("inside the if statement")
+        player2_marker = "O"
+        print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+
+    if(player_two_response == 3):
+        print("inside the if statement")
+        player2_marker = "O"
+        print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+
+    if(player_two_response == 4):
+        print("inside the if statement")
+        # player2_marker = "O"
+        print("this should be what player one chose as their character", player2_marker)
+        # print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+
+    if(player_two_response == 5):
+        print("inside the if statement")
+        player2_marker = "O"
+        print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+
+    if(player_two_response == 6):
+        print("inside the if statement")
+        player2_marker = "O"
+        print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+
+    if(player_two_response == 7):
+        print("inside the if statement")
+        player2_marker = "O"
+        print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+    
+    if(player_two_response == 8):
+        print("inside the if statement")
+        player2_marker = "O"
+        print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
+
+    if(player_two_response == 9):
+        print("inside the if statement")
+        player2_marker = "O"
+        print("this should be O", player2_marker)
+        board_list[player_two_response] = player2_marker
     
     print("updated board")
     print_board(board_list)
 
-# def location_confirm(player_one_response, location):
-    
-    
 
 #Collect smaller functions into this function
-def main_function(response_one, player1_marker, location, player2_marker, player_one_response, filled_board):
+def main_function(response_one, player1_marker, player2_marker, player_one_response, filled_board):
+
+
+# def main_function():
+
+#Think about changing the variable, response_one, passed to change_marker to, response, to see if the program still runs without errros
     user_input(response_one, player1_marker,player2_marker)
     print_board(filled_board)
-    get_location(filled_board, player1_marker, player2_marker)
-    # change_marker(player_one_response, filled_board, location, player1_marker, player2_marker)
-    # change_marker()
+    l = get_location(filled_board, player1_marker, player2_marker)
+# change_marker(player_one_response, filled_board, location, player1_marker, player2_marker)
+    change_marker_player_one(filled_board, player1_marker, l)
+    change_marker_player_two(filled_board, player2_marker, l)
 
 
 #DRIVER CODE
-main_function(response_one, player1_marker, player2_marker, player_one_response, location, filled_board)
+main_function(response_one, player1_marker, player1_marker, player_one_response, filled_board)
 
     
 
